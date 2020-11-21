d7ea87f484b608baf890c56ed4879b82b780c136
noriyotcp <noriyo.akita@gmail.com>
2020-11-21 17:30:06 +0900

    Add getCars and getLastItemId to clientStorage.js

--- car-deals-2/js/clientStorage.js
+++ car-deals-2/js/clientStorage.js
@@ -2,7 +2,26 @@
 const carsInstance = localforage.createInstance({
   name: "cars"
 });
+let lastIndex = -1;
 
 export const addCars = async (newCars) => {
   await carsInstance.setItems(newCars);
+};
+
+export const getCars = async () => {
+  // keys を降順で取り出す。この時点で length チェックを行っている
+  const keys = (await carsInstance.keys()).reverse();
+  if (lastIndex >= keys.length) {
+    return;
+  }
+  // keys から 3件ずつ取り出す。keys の内容は変更される
+  const results = await carsInstance.getItems(keys.splice(lastIndex + 1, 3));
+  lastIndex += 3;
+  // results (DB の values) をオブジェクトの values にしつつ降順にする
+  return Object.values(results).reverse();
+};
+
+export const getLastItemId = async () => {
+  const keys = (await carsInstance.keys()).reverse();
+  return keys[lastIndex];
 };
