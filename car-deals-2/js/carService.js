ced39dbec3c1689f89db94b7649f8dd1d2480008
noriyotcp <noriyo.akita@gmail.com>
2020-11-21 09:05:36 +0900

    Dynamic rendering cards

--- car-deals-2/js/carService.js
+++ car-deals-2/js/carService.js
@@ -1,8 +1,10 @@
 import { API_URL_LATEST } from "./constants.js";
+import { appendCars } from "./template.js";
 
 export const loadCars = async () => {
   const response = await fetch(API_URL_LATEST);
   const data = await response.json();
+  appendCars(data.cars);
   // eslint-disable-next-line no-console
   console.log(data);
 };
