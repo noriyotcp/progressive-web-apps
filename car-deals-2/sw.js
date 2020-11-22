0a353183c6a792086721d7e506102ffaa709d79d
noriyotcp <noriyo.akita@gmail.com>
2020-11-22 10:27:25 +0900

    Create cache and add files on install event

--- car-deals-2/sw.js
+++ car-deals-2/sw.js
@@ -1,7 +1,28 @@
 "use strict";
 
+const carDealsCacheName = "carDealsCacheV1";
+const carDealsCacheFiles = [
+  "https://cdn.jsdelivr.net/npm/pwacompat@2.0.17/pwacompat.min.js",
+  "https://cdn.jsdelivr.net/gh/bstavroulakis/progressive-web-apps/resources/localforage.js",
+  "js/app.js",
+  "js/carPageService.js",
+  "js/carService.js",
+  "js/clientStorage.js",
+  "js/constants.js",
+  "js/swRegister.js",
+  "js/template.js",
+  "/",
+  "index.html",
+  "style.css",
+];
+
 self.addEventListener("install", (event) => {
   console.log("From SW: Install Event");
+  const preCache = async () => {
+    const cache = await caches.open(carDealsCacheName);
+    return cache.addAll(carDealsCacheFiles);
+  };
+  event.waitUntil(preCache());
 });
 
 self.addEventListener("activate", (event) => {
