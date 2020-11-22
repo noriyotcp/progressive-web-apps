b7301ab37c4c20553be80001a5190029703a8466
noriyotcp <noriyo.akita@gmail.com>
2020-11-22 13:19:49 +0900

    Clear cache when Service Worker is activate

--- car-deals-2/sw.js
+++ car-deals-2/sw.js
@@ -1,6 +1,8 @@
 "use strict";
 
-const carDealsCacheName = "carDealsCacheV1";
+const carDealsCacheName = "carDealsCacheV2";
+const carDealsCachePagesName = "carDealsCachePagesV1";
+const carDealsCache = [carDealsCacheName, carDealsCachePagesName];
 const carDealsCacheFiles = [
   "https://cdn.jsdelivr.net/npm/pwacompat@2.0.17/pwacompat.min.js",
   "https://cdn.jsdelivr.net/gh/bstavroulakis/progressive-web-apps/resources/localforage.js",
@@ -27,4 +29,14 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("activate", (event) => {
   console.log("From SW: Activate Event");
+  const clearCache = async () => {
+    const keys = await caches.keys();
+    keys.forEach(async k => {
+      if (carDealsCache.includes(k)) {
+        return;
+      }
+      await caches.delete(k);
+    })
+  };
+  event.waitUntil(clearCache());
 });
